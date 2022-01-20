a8f5af8c146a27244d960b0dcd74c1894b1833ea
Shueyi <93110288+Shueyi@users.noreply.github.com>
2022-01-20 19:07:25 +0800

    Add files via upload

---
+++ signin/dbh.php
@@ -0,0 +1,13 @@
+<?php
+
+    $servername= "localhost";
+    $username= "root";
+    $password= "";
+    $dbname= "airost_project";
+
+    $conn= mysqli_connect($servername, $username, $password, $dbname);
+?>
+    
+      
+
+        
